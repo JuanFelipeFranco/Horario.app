8bb9ecc2b6e0f79f484e4a55a3d5751212c82756
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-14 16:06:27 -0500

    update controller.

--- Horario.app/src/main/java/com/felipe/Horario/app/controllers/AppController.java
+++ Horario.app/src/main/java/com/felipe/Horario/app/controllers/AppController.java
@@ -1,5 +1,6 @@
 package com.felipe.Horario.app.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +14,11 @@ import java.util.Map;
 public class AppController {
 
     @GetMapping("/foo")
-    public ResponseEntity<?> foo(){
+    public ResponseEntity<?> foo(HttpServletRequest request){
         Map<String, Object> data = new HashMap<>();
         data.put("title","Bienvenidos al sistema de atencion");
         data.put("time",new Date());
+        data.put("message",request.getAttribute("message"));
         return ResponseEntity.ok(data);
     }
 }
